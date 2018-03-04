3919a73df07a727ccbdd72e87b96f55db6f1cb95
Casper Smits <caspersmits11@outlook.com>
2018-03-04 16:59:14 +0100

    Updated settings code

--- app/js/window.js
+++ app/js/window.js
@@ -2,8 +2,6 @@ const $ = require('jQuery');
 const { ipcRenderer } = require('electron');
 const click = require('./js/controls/click');
 
-const test = $('.settings');
-
 click.init();
 
 $(document).ready(function() {
@@ -19,56 +17,68 @@ ipcRenderer.on('render-completed', function(event, arg) {
     $('#target-image').attr('src', '../renders/' + arg);
 });
 
-function buildSettings(settings) {
+function buildSettings(settings, settingsType) {
 
-    var html = $('<form class="uk-form-horizontal"></form>');
+    var form = $('<form class="uk-form-horizontal settings"></form>');
 
-    settings.forEach((element) => {
+    Object.keys(settings).forEach(function (key) {
 
-        var input = null;
+        var element = settings[key];
+        var input = getInputElem(element);
 
-        switch (element.type) {
-            case 'integer':
-                input = $('<input type="number" step="1" pattern="\d+" />');
-                break;
-
-            case 'float':
-                input = $('<input />');
-            default:
-
-        }
-
-        input.attr('id', element.identifier);
-        input.attr('class', 'uk-input');
-
-        html.append(
+        form.append(
             $('<div class="uk-margin"></div>').append(
                 $('<label class="uk-form-label" for="' + element.identifier + '"></label').text(element.description)
             ).append(
                 $('<div class="uk-form-controls"></div>').append(input)
             )
         );
-
     });
 
-    test.html(html);
+    if (settingsType == 'fractal') {
+        $('.fractal-settings').html(form);
+    } else if (settingsType == 'colour') {
+        $('.colour-scheme-settings').html(form);
+    }
+}
+
+function getInputElem(element) {
+
+    var input = null;
+
+    switch (element.type) {
+        case 'integer':
+            input = $('<input type="number" step="1" pattern="\d+" />');
+            break;
+
+        case 'float':
+            input = $('<input />');
+        default:
+    }
+
+    input.attr('id', element.identifier);
+    input.attr('class', 'uk-input user-input');
+    input.attr('oninput', 'update()');
+
+    return input;
+}
+
+function update() {
 
 }
 
 // TODO Move these somewhere else
-var mandelbrotSettings = [
-    {
-        "identifier": "max_iterations",
+var mandelbrotSettings = {
+    "max_iterations": {
         "description": "Maximum number of iterations",
         "value": 512,
         "type": "integer"
     },
-    {
-        "identifier": "escape_value",
+    "escape_value": {
         "description": "Escape value",
         "value": 2.0,
         "type": "float"
     }
-]
+}
 
-buildSettings(mandelbrotSettings);
+buildSettings(mandelbrotSettings, 'fractal');
